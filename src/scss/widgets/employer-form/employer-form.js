7702e7ae27b1b0bc214e59363fd609b651c36379
Mohammad Sharifi <mmdsharifi@yahoo.com>
2016-09-06 10:09:20 +0430

    remove: src folder

--- src/scss/widgets/employer-form/employer-form.js
+++
@@ -1,9 +0,0 @@
-{
-  let checkbox = document.querySelector('.employer-form .form__action .mdl-checkbox__input'),
-	button = document.querySelector('.employer-form .form__action .mdl-button');
-  button.disabled = !checkbox.checked;
-
-  checkbox.addEventListener('change', () => {
-    button.disabled = !checkbox.checked;
-  });
-}
